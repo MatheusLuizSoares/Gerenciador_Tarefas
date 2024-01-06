b02590cccc47546435dc0579f16d759b9111f26e
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-06 18:04:28 -0300

    privatização

--- setup/settings.py
+++ setup/settings.py
@@ -1,7 +1,8 @@
 
 
 from pathlib import Path
-
+from decouple import config,Csv
+from dj_database_url import parse as db_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-isos60nb=jh_8r=p5$r*-=wo$k)1p(!%xx@hd$zg)d1@ygt58o'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool,default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
 # Application definition
@@ -65,12 +66,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+    "DATABASE_URL", 
+    default=f'sqlite:///{BASE_DIR /"db.sqlite3"}', 
+    cast=db_url,)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
