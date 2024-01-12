4fba382e464438f6cd6447185ec96aa1a21c8cf6
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-12 15:16:06 -0300

    continuando que eu escrevi no ultimo commit

--- todos/migrations/0001_initial.py
+++ todos/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('dealine', models.DateTimeField()),
+                ('deadline', models.DateTimeField()),
                 ('finished', models.DateField(null=True)),
             ],
         ),
--- todos/views.py
+++ todos/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from .models import Todo
 def todo_list(request):
  todos=Todo.objects.all()
- return render(request,"todos/todo_list.html")
+ return render(request,"todos/todo_list.html",{'todos': todos})
