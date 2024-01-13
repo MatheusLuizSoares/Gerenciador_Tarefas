9e9802e949a7e39d039507e73848004fe4174232
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-13 10:17:33 -0300

    refromulando a viws

--- todos/models.py
+++ todos/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Todo(models.Model):
    title= models.CharField(max_length=100,null=False,blank=False)
    created_at= models.DateTimeField(auto_now_add=True,null=False, blank=False)
-   dealine=models.DateTimeField(null=False,blank=False)
+   dealine=models.DateField(null=False,blank=False)
    finished=models.DateField(null=True)
