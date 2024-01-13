7564585307fe57040b007f777529c75053da9cbe
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-13 11:29:26 -0300

    colocando o bootstrap

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from todos.views import TodoListView
+from todos.views import TodoListView,TodoCreateViws
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TodoListView),
+    path('',TodoListView.as_view(), name="todo_list"),
+    path("create",TodoCreateViws.as_view(),name="todo_create"),
     ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Todo
 
 class TodoListView(ListView):
   model=Todo
+
+class TodoCreateViws(CreateView):
+  model = Todo
+  fields=["title", "deadline"]
+  success_url=reverse_lazy("todo_list")
+  
