f80747af344a40d4ba7f238ccb562e4f6932261a
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-14 17:04:24 -0300

    Fim do projeto, foi projeto pequeno,porém ele foi util para aprender algumas coisa do django

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,10 @@
+from datetime import date
 from django.shortcuts import render
-from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView,View
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Todo
-
+from django.shortcuts import get_object_or_404,redirect
 class TodoListView(ListView):
   model=Todo
 
@@ -21,3 +22,9 @@ class TodoDeleteView(DeleteView):
   model=Todo
   success_url=reverse_lazy("todo_list")
   
+  
+class TodoCompleteView(View):
+  def get(Self,request,pk):
+    todo=get_object_or_404(Todo, pk=pk)
+    todo.mark_has_complete()
+    return redirect("todo_list")
